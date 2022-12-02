b5d1dad77daa77d3f6ff35a1ea98a45d99041b88
z0s3r77 <ipopdue24@gmail.com>
2022-12-02 01:29:54 +0100

    fix: Se ha encapsulado la peticion en requesToMongoApi
    He definido la función requestToMongoApi para que nos dé como resultado un string con los documentos de las bicis. Posteriormente esto se convertirá en un JSON

--- src/db/conexionApiMongo.py
+++ src/db/conexionApiMongo.py
@@ -1,27 +1,36 @@
 import requests
 import json
-url = "https://data.mongodb-api.com/app/data-cozpc/endpoint/data/v1/action/find"
-payload = json.dumps({
-    "collection": "bikes",
-    "database": "BookingBike",
-    "dataSource": "Sandbox",
-    "filter": {"Brand":"shimano"},
-    "projection":{
-      "_id":1,
-      "Brand":1,
-      "Model":1,
-      "Price":1,
-      "Status":1
-      }
-})
-headers = {
-  'Content-Type': 'application/json',
-  'Access-Control-Request-Headers': '*',
-  'api-key': 'yvkotEFnlbULNH9Zm14y8LSGZMYcbuA26YF5P9nZlXDpG4LEcEhlJYI3tALDVh5q',
-  'Accept': 'application/ejson' 
-}
-response = requests.request("POST", url, headers=headers, data=payload)
-result = (response.text)
-result = result.replace('{"documents":',"")
-result = result.replace('}',"",-1)
-print (result)
+
+# Creamos una función que nos permite hacer una petición de datos a MongoAtlas
+# En esta petición(request) obtenemos como respuesta la colección de bikes
+
+def requestToMongoApi():
+  
+  url = "https://data.mongodb-api.com/app/data-cozpc/endpoint/data/v1/action/find"
+  payload = json.dumps({
+      "collection": "bikes",
+      "database": "BookingBike",
+      "dataSource": "Sandbox",
+      "filter": {"Brand":"shimano"},
+      "projection":{
+        "_id":1,
+        "Brand":1,
+        "Model":1,
+        "Price":1,
+        "Status":1
+        }
+  })
+  headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Request-Headers': '*',
+    'api-key': 'yvkotEFnlbULNH9Zm14y8LSGZMYcbuA26YF5P9nZlXDpG4LEcEhlJYI3tALDVh5q',
+    'Accept': 'application/ejson' 
+  }
+  response = requests.request("POST", url, headers=headers, data=payload)
+  result = (response.text)
+  result = result.replace('{"documents":',"")
+  result = result.replace('}',"",-1)
+  
+  
+  return result
+
