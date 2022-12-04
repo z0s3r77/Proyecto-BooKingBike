916ee04a11b52efaf0f9d6385f5a129f3618f71b
mvidalt <mvidalt@cifpfbmoll.eu>
2022-12-04 16:29:38 +0100

    feat: añadimos a main la pagina contacto
    añadimos a main.py las funciones necesarias para generar la pagina de contacto

--- src/views/main.py
+++ src/views/main.py
@@ -12,88 +12,126 @@ def html_head(title='Booking Bike', description='Booking Bike'):
     # Obtenemos la fecha actual
     date = datetime.today().strftime('%Y-%m-%d')
     head = f"""<!DOCTYPE html>
-    <html lang="es">
+<html lang="es">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="author" content="Miguel & Sebastian">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name='description' content='{description}'>
-        <meta name="keywords" content="MTB, Ebike , CityBike">
-        <meta http-equiv="last-modified" content='{date}'>
+        <meta name='description' content='Pagina de inicio de BookingBike'>
+        <meta name="keywords" content="MTB, Ebike , CityBike, Bikes , Renting">
+        <meta http-equiv="last-modified" content='2022-12-01'>
         <link rel="icon" href="https://cdn-icons-png.flaticon.com/512/3714/3714324.png">
-        <title>{title}</title>
+        <title>Contacto</title>
     </head>"""
     print(head)
 
 
 # BODY
 
-def body_pagina_principal():
-    body = """<body>
-    <header>
-        <h1>BookingBike</h1>
-    </header>
-    <nav>
-        <ul>
-            <li><a href="listadototalbicis.html">Todas las bicis</a></li>
-            <li><a href="listadomarcabicis.html"> Marcas disponibles</a></li>
-            <li><a href="listadotipobicis.html"> Modelos disponibles</a></li>            
-            <li><a href="listadobicisporzona.html"> Zonas disponibles </a></li>            
-            <li><a href="#contacto">Contacto</a></li>
-        </ul>
-    </nav>
-    <main>
-        <div>
-            <h4>¿Quienes somos?</h4>
-            <p>Somos una empresa fundada en España en el año 2022 con el objetivo de ayudar a la ciudadania
-                y al medio ambiente a ir hacia un futuro mejor y ecosostenible. 
-            </p>
-        </div>
-        <div>
-            <h4>¿Que hacemos?</h4>
-            <p>Hemos creado una plataforma donde los ciudadanos pueden comprobar la disponibilidad de bicicletas 
-                en alquiler en un área determinada. Para esto, todas las empresas <u>certificadas</u> de alquiler
-                de bicicletas que estén interesadas pueden volcar su catálago con nosotros.
-            </p>
-        </div>
-        <div>
-            <h4>¿Como reservar?</h4>
-            <p> <b>!Sencillo!</b> Tan solo debes dirigirte a uno de los enlaces que tienes disponibles en nuestra barra de navegación.
-                Puedes ver todo el <i>"arsenal"</i> de bicicletas al completo o filtrar por modelo o marca. Una vez, seleccionada
-                la que más te conviene, tan solo haz <u>clic</u> en <b>Reservar</b> y te indicaremos los siguientes pasos.  
-            </p>
-        </div>
-        <div>
-            <h4>¿Por que BookingBike?</h4>
-            <p>Porque ahora, alquilar tu bicicleta favorita para cada época del año es más sencillo de lo que te imaginas.
-                Nosotros te mostramos todo el cátalogo disponible en tu zona, tú eres el encargado o encargada de seleccionar
-                la que mejor se adapte a tí y en tan solo unos días la tendrás en la puerta de tu casa. <b>Así de simple!</b>
-            </p>
-        </div>
-    </main>"""
-    print(body)
+class body():
+    def body_pagina_principal():
+        body ="""    <body>
+        <header>
+            <h1>BookingBike</h1>
+        </header>
+        <nav>
+            <ul>
+                <li><a href="listadototalbicis.html">Todas las bicis</a></li>
+                <li><a href="listadomarcabicis.html"> Marcas disponibles</a></li>
+                <li><a href="listadotipobicis.html"> Modelos disponibles</a></li>            
+                <li><a href="listadobicisporzona.html"> Zonas disponibles </a></li>            
+                <li><a href="#contacto">Contacto</a></li>
+            </ul>
+        </nav>
+        <main>
+            <div>
+                <h4>¿Quienes somos?</h4>
+                <p>Somos una empresa fundada en España en el año 2022 con el objetivo de ayudar a la ciudadania
+                    y al medio ambiente a ir hacia un futuro mejor y ecosostenible. 
+                </p>
+            </div>
+            <div>
+                <h4>¿Que hacemos?</h4>
+                <p>Hemos creado una plataforma donde los ciudadanos pueden comprobar la disponibilidad de bicicletas 
+                    en alquiler en un área determinada. Para esto, todas las empresas <u>certificadas</u> de alquiler
+                    de bicicletas que estén interesadas pueden volcar su catálago con nosotros.
+                </p>
+            </div>
+            <div>
+                <h4>¿Como reservar?</h4>
+                <p> <b>!Sencillo!</b> Tan solo debes dirigirte a uno de los enlaces que tienes disponibles en nuestra barra de navegación.
+                    Puedes ver todo el <i>"arsenal"</i> de bicicletas al completo o filtrar por modelo o marca. Una vez, seleccionada
+                    la que más te conviene, tan solo haz <u>clic</u> en <b>Reservar</b> y te indicaremos los siguientes pasos.  
+                </p>
+            </div>
+            <div>
+                <h4>¿Por que BookingBike?</h4>
+                <p>Porque ahora, alquilar tu bicicleta favorita para cada época del año es más sencillo de lo que te imaginas.
+                    Nosotros te mostramos todo el cátalogo disponible en tu zona, tú eres el encargado o encargada de seleccionar
+                    la que mejor se adapte a tí y en tan solo unos días la tendrás en la puerta de tu casa. <b>Así de simple!</b>
+                </p>
+            </div>
+        </main>"""
+        print(body)
 
+    def body_contacto():
+        body = """    <body>
+        <header>
+            <h1>Pagina de contacto</h1>
+        </header>
+        <main>
+            <section>
+                <form>
+                    <fieldset>
+                        <legend>Contacta con nosotros</legend>
+                        <label for='nombre'> Nombre: </label>
+                        <input type='text' name='nombre' id='nombre'><br><br>
+                        <label for='correo'> Correo: </label>
+                        <input type='text' name='correo' id='correo'><br><br>
+                        <label for='mensaje'> Mensaje: </label><br>
+                        <textarea type='text' name='mensaje' id='mensaje'></textarea><br>
+                        <input type='submit' value='enviar'>
+                        <input type='reset' value='borrar'>
+                    </fieldset>
+                </form>
+            </section>
+        </main>"""
+        print(body)    
 
 # FOOTER
 
 def footer():
-    footer = """<footer>
-        <div>
-            <p><i>Pagina realizada por @MiguelVidalt & @SebastianEstacio</i></p>
-        </div>
-    </footer>
+    footer = """        <footer>
+            <p>Esta página es de @Miguel & @Sebastian</p>
+            <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><img alt="Licencia Creative Commons"
+                    style="border-width:0" src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png" /></a><br />Esta obra
+            está
+            bajo una <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">Licencia Creative Commons
+                Atribución-NoComercial 4.0 Internacional</a>.
+        </footer>
     </body>
-    </html>"""
+</html>"""
     print(footer)
 
-#GENERANDO HOME    
+# GENERANDO PAGINAS
+
 
 def pagina_principal():
     sys.stdout = open('home.html', 'w', encoding="UTF-8")
-    html_head(title="Pagina principal - BookingBike", description='Pagina de inicio de BookingBike')
-    body_pagina_principal()
+    html_head(title="Pagina principal - BookingBike",
+              description='Pagina de inicio de BookingBike')
+    body.body_pagina_principal()
+    footer()
+    sys.stdout.close()
+
+def contacto():
+    sys.stdout = open('contacto.html', 'w', encoding="UTF-8")
+    html_head(title="Pagina de contacto - BookingBike",
+              description='Pagina de para contactar con la organización')
+    body.body_contacto()
     footer()
     sys.stdout.close()
 
 pagina_principal()
+contacto()
