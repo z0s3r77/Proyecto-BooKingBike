e460a5ff4c359116aacc6de465aedc50832b3fd0
z0s3r77 <ipopdue24@gmail.com>
2022-12-10 10:54:42 +0100

    perf: Añadido Markers Pytest
    Se ha podido añadir Markers. Esto no se podía ya que no entendía porque no me dejaba crear el fichero pytest.ini

--- test/test_DB_conexionApiMongo.py
+++ test/test_DB_conexionApiMongo.py
@@ -1,6 +1,7 @@
 import pytest
 from src.db.conexionApiMongo import requestToMongoApi
 
+@pytest.mark.test_response
 def test_requestToMongoApi():
     #Comrpobamos que la respuesta sea un str
     assert type(requestToMongoApi()) == str
