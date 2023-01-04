062323321b56091ac068557d30925040402d3b93
z0s3r77 <ipopdue24@gmail.com>
2023-01-04 17:21:21 +0100

    actualizado gitPages

--- src/GITauto/autoGit.py
+++ src/GITauto/autoGit.py
@@ -3,4 +3,4 @@ import random
 
 
 subprocess.run(["git", "add", "."])
-subprocess.run(["git", "commit" , "-m", "actualizado gitPages" ], capture_output=False)
+subprocess.run(["git", "commit" , "-m", "actualizado gitPages" ], shell=False)
