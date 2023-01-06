0d919a9ceef0c044b389b2963aca254b33b1da02
z0s3r77 <ipopdue24@gmail.com>
2023-01-06 13:39:48 +0100

    FUnciona

--- src/GITauto/autoGit.py
+++ src/GITauto/autoGit.py
@@ -5,7 +5,15 @@ import random
 def gitPush(message):
 
     subprocess.run(["git", "add", "."])
-    subprocess.run(["git", "commit" , "-m", message ], stdout=subprocess.DEVNULL)
-    subprocess.run(["git", "push",])
+
+    if not message:
+        subprocess.run(["git", "commit" , "-m", "base de datos actualizada" ], stdout=subprocess.DEVNULL)
+    else:
+        subprocess.run(["git", "commit" , "-m", message ], stdout=subprocess.DEVNULL)
+
+    subprocess.run(["git", "push","origin", "master"])
+
+
+gitPush('FUnciona')
 
 # Como lo subo a master?
